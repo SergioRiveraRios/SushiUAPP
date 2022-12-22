7b90e0a08c4641bbf8d6d1f15166e3273d9bfc87
Sergio Rivera <sergioriverarios5@gmail.com>
2022-12-22 00:24:28 -0600

    Added component

---
+++ sushiUser/src/components/RestaurantItem/index.js
@@ -0,0 +1,36 @@
+
+import { StyleSheet, Text, View,Image } from 'react-native';
+
+
+const RestaurantItem = ({restaurant})=>{
+    return (
+      <View style={styles.restaurantContainer}>
+             <Image source={{uri:restaurant.image,}} style={styles.image}/>
+             <Text style={styles.Title}>{restaurant.name}</Text>
+             <Text style={styles.subTitle}>{restaurant.deliveryFee}</Text>
+        </View>
+    )
+  }
+
+  export default RestaurantItem
+  const styles = StyleSheet.create({
+    restaurantContainer:{
+      width:"100%",
+      padding: 10
+    },
+    image:{
+      width:"100%",
+      aspectRatio: 5/3,
+      marginBottom:5
+    },
+    Title:{
+      fontSize:16,
+      fontFamily: "bold",
+      marginVertical:5
+    },
+    subTitle:{
+      fontSize:12,
+      color:"grey"
+    }
+  });
+  
