e5f6fc64546010f4c684b795954120615e319920
Sergio Rivera <sergioriverarios5@gmail.com>
2023-01-08 17:22:02 -0600

    fixed navigation path error

--- sushiUser/src/navigation/index.js
+++ sushiUser/src/navigation/index.js
@@ -1,4 +1,21 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 
+//Import all screens 
+import HomeScreen from '../screens/HomeScreen'
+import BasketScreen from '../screens/basketScreen'
+import OrderDetails from '../screens/orderDetailes'
+import SushiDetailScreen from '../screens/sushiDetailScreen'
+import SushiList from '../screens/sushiList'
 
 const Stack= createNativeStackNavigator()
+
+const  RootNavigator=()=>{
+    return(
+        <Stack.Navigator initialRouteName='Sushi'>
+            <Stack.Screen name="Home" component={HomeScreen}/>
+            <Stack.Screen name="Sushi" component={SushiDetailScreen} options={{headerShown:false}}/>
+        </Stack.Navigator>
+    )
+}
+
+export default RootNavigator
