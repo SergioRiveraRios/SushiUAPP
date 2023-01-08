89852a908f20a18071517dc2f64b32e680d667aa
Sergio Rivera <sergioriverarios5@gmail.com>
2023-01-08 14:04:28 -0600

    added orderdetails screen

---
+++ sushiUser/src/screens/basketScreen/index.js
@@ -0,0 +1,88 @@
+import {View,Text,StyleSheet, FlatList} from 'react-native'
+import restarurants from '.././../../assets/data/restaurants.json'
+import BasketDishItem from '../../components/BasketDishItem'
+import { AntDesign } from '@expo/vector-icons'; 
+import { useState } from 'react';
+import { bool } from 'prop-types';
+
+const dish = restarurants[0].dishes[0]
+const restarurant= restarurants[0]
+
+
+const BasketScreen = ()=>{
+    const [quantity,setQuantity]= useState(1)
+    const getTotal=()=>{return (dish.price * quantity).toFixed(2)}
+
+    return(
+        <View style={styles.page}>
+            <AntDesign name="leftcircleo" size={30} color="black" />
+            <Text style={styles.title}>Tu carrito</Text>
+
+            <Text style={styles.descrpition}>{dish.description}</Text>
+
+            <FlatList data={restarurant.dishes} renderItem={({item})  => <BasketSushiItem basketSushi={item} />} />
+
+
+            <View style={styles.separator}/>
+
+            
+            <View style={styles.buttonContainer}>
+                <Text style={styles.buttonAdd}> Add {quantity} to basket (${getTotal()}) </Text>
+            </View>
+        </View>
+    )
+}
+
+export default BasketScreen
+
+const styles = StyleSheet.create({
+    page:{
+        flex:1,
+        width:"100%",
+        paddingVertical:30,
+        paddingLeft:15
+    },
+    title:{
+        paddingLeft:15,
+        fontSize:30,
+        fontWeight:"500"
+    },
+    descrpition:{
+        paddingLeft:15,
+        color:"grey"
+    },
+    separator:{
+        height:1,
+        backgroundColor:"lightgrey",
+        marginVertical:10
+    },
+    row:{
+        flexDirection:"row",
+        alignItems:"center",
+        marginTop:40
+    },
+    quantity:{
+        fontSize:25,
+        marginHorizontal:20
+    },
+    buttonContainer:{
+        backgroundColor:"black",
+        padding:20,
+        alignItems:"center",
+        marginTop:"auto"
+    },
+    buttonAdd:{
+        color:"white",
+        fontWeight:"600"
+    },
+    price:{
+        marginLeft:"auto"
+    },
+    tucarrito:{
+        backgroundColor:"grey",
+        paddingHorizontal:5,
+        paddingVertical:2,
+        marginRight:10,
+        borderRadius:5
+    }
+})
---
+++ sushiUser/src/screens/orderDetailes/index.js
@@ -0,0 +1,75 @@
+import {  } from "module";
+import { View,Text,StyleSheet,Image, FlatList } from "react-native";
+
+import orders from '../../../assets/data/orders.json'
+
+import restarurants from '.././../../assets/data/restaurants.json'
+
+import SushiListItem from '../../components/sushiListItem/index'
+
+import BasketDishItem from '../../components/BasketDishItem'
+const order =orders[0]
+
+const OrderDetailsHeader =() =>{
+    return(
+        <View style={styles.restaurantContainer}>
+             <Image source={{uri: order.Restaurant.image,}} style={styles.image}/>
+        <View style={styles.row}>
+
+          <View>
+            <Text style={styles.Title}>{order.Restaurant.name}</Text>
+            <Text style={styles.subTitle}>{order.status}  2 Days ago</Text>
+
+            <Text style={styles}> Your order</Text>
+          </View>
+        </View>
+      </View>
+    )
+}
+const OrderDetails =()=>{
+    return(
+        
+        <FlatList 
+        ListHeaderComponent={OrderDetailsHeader}
+        data={restarurants[0].dishes} 
+                   renderItem={({item})=><BasketDishItem basketSushi={item}/> }/>
+    )
+}
+
+
+export default OrderDetails
+
+const styles = StyleSheet.create({
+    restaurantContainer:{
+      width:"100%",
+      marginVertical:10
+    },
+    image:{
+      width:"100%",
+      aspectRatio: 5/3,
+      marginBottom:5
+    },
+    Title:{
+      fontSize:20,
+      fontFamily: "bold",
+      marginVertical:5
+    },
+    subTitle:{
+      fontSize:12,
+      color:"grey"
+    },
+    row:{
+      flexDirection:'row',
+      alignItems:"center"
+    },
+    rating:{
+      marginLeft:"auto",
+      backgroundColor:"Lighgray",
+      width:25,
+      height:25,
+      alignItems:"center",
+      justifyContent:"center",
+      borderRadius:20
+    }
+  });
+  
