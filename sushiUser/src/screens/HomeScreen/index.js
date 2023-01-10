aba51f7ed6367dc7007a93125120dff8d6986697
Sergio Rivera <sergioriverarios5@gmail.com>
2023-01-10 00:00:44 -0600

    modified root navigation entry

--- sushiUser/src/screens/HomeScreen/index.js
+++ sushiUser/src/screens/HomeScreen/index.js
@@ -3,7 +3,7 @@ import { StyleSheet,FlatList, View } from 'react-native';
 import RestaurantItem from '../../../src/components/RestaurantItem/index'
 import restaurants from '../../../assets/data/restaurants.json'
 
-
+//Change Restaurant Item for Menu Items
 const HomeScreen=()=> {
   return (
     <View style={styles.page}>
