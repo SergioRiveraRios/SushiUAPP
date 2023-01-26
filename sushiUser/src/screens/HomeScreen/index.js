c14f573097983a547cf7b46fdd262ba795e5d37f
Sergio Rivera <sergioriverarios5@gmail.com>
2023-01-26 16:17:58 -0600

    fixed auth

--- sushiUser/src/screens/HomeScreen/index.js
+++ sushiUser/src/screens/HomeScreen/index.js
@@ -1,20 +1,30 @@
 
-import { StyleSheet,FlatList, View } from 'react-native';
+import { StyleSheet, FlatList, View, Text, Item } from 'react-native';
 import RestaurantItem from '../../../src/components/RestaurantItem/index'
 import restaurants from '../../../assets/data/restaurants.json'
 import menu from '../../../assets/data/menu.json'
-
+import { useEffect, useState } from 'react';
+import { DataStore } from 'aws-amplify';
+import { MenuItem } from '../../models';
+import { ItemCategoria } from '../../models';
 //Change Restaurant Item for Menu Items
-const HomeScreen=()=> {
+
+
+const HomeScreen = () => {
+  const [menu, setMenu] = useState([])
+  useEffect(() => {
+    DataStore.query(ItemCategoria).then((results) => setMenu(results))
+  })
+  
   return (
     <View style={styles.page}>
       <FlatList 
           data={menu} 
-          renderItem={ ({item})=> <RestaurantItem menu={item} />}
+          renderItem={ ({item,index})=> <RestaurantItem menu={item} index={index} />}
           showsVerticalScrollIndicator={false}
       />
     </View>
-      
+
   );
 }
 export default HomeScreen
