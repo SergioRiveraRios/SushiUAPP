70ff723ebf12136ce1f84b9a03d1a7fdcb1062cc
Sergio Rivera <sergioriverarios5@gmail.com>
2023-01-26 16:17:29 -0600

    added multiple routes

--- sushiUser/src/navigation/index.js
+++ sushiUser/src/navigation/index.js
@@ -8,10 +8,12 @@ import OrderDetails from '../screens/orderDetailes'
 import SushiDetailScreen from '../screens/sushiDetailScreen'
 import SushiList from '../screens/sushiList'
 import ProfileScreen from '../screens/profileScreen';
+import AccountScreen from '../screens/AccountScreen'
 import { TabActions } from '@react-navigation/native'
 
 const Stack= createNativeStackNavigator()
 const homeStack=createNativeStackNavigator()
+const ProfileStack=createNativeStackNavigator()
 const bottomTab=createMaterialBottomTabNavigator()
 
 const  RootNavigator=()=>{
@@ -27,7 +29,7 @@ const HomeTabs=()=>{
         <bottomTab.Navigator>
             <bottomTab.Screen name='Inicio' component={HomeStackNavigator} options={{tabBarIcon:()=><AntDesign name="home" size={24} color="black" />}}/>
             <bottomTab.Screen name='Carrito' component={BasketScreen} options={{tabBarIcon:()=><AntDesign name="shoppingcart" size={24} color="black" />}}/>
-            <bottomTab.Screen name='Perfil' component={ProfileScreen} options={{tabBarIcon:()=><AntDesign name="profile" size={24} color="black" />}}/>
+            <bottomTab.Screen name='Perfil' component={ProfileStackNavigator} options={{tabBarIcon:()=><AntDesign name="profile" size={24} color="black" />}}/>
         </bottomTab.Navigator>
     )
 }
@@ -40,6 +42,14 @@ const HomeStackNavigator=()=>{
             <homeStack.Screen name="SushiList" component={SushiList} screenOptions={{headerShown:false}}></homeStack.Screen>
             <homeStack.Screen name="BasketScreen" component={BasketScreen}></homeStack.Screen>
         </homeStack.Navigator>
+    )
+}
+const ProfileStackNavigator=()=>{
+    return(
+        <ProfileStack.Navigator>
+            <ProfileStack.Screen name='Perfil' component={ProfileScreen}></ProfileStack.Screen>
+            <ProfileStack.Screen name='Mi Cuenta' component={AccountScreen}></ProfileStack.Screen>
+        </ProfileStack.Navigator>
     )
 }
 export default RootNavigator
