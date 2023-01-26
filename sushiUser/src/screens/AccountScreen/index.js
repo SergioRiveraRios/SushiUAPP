14c655333444accffd8dd31224d8bdffe63bddba
Sergio Rivera <sergioriverarios5@gmail.com>
2023-01-26 01:36:52 -0600

    added account screen

---
+++ sushiUser/src/screens/AccountScreen/index.js
@@ -0,0 +1,12 @@
+import {View,Text} from 'react-native'
+
+
+const AccountScreen=()=>{
+    return(
+        <View>
+            <Text>ASDDSD</Text>
+        </View>
+    )
+}
+
+export default AccountScreen
