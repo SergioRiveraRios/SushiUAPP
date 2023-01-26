7ea69f451d363588635bbb9f63ff7ee9b1de8adb
Sergio Rivera <sergioriverarios5@gmail.com>
2023-01-26 16:20:09 -0600

    added auth logic still need pol

--- sushiUser/App.js
+++ sushiUser/App.js
@@ -1,17 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
-import {NavigationContainer} from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native'
 import RootNavigator from './src/navigation';
-export default function App() {
+import { Amplify, Analytics } from 'aws-amplify'
+import config from '../sushiUser/aws-exports'
+import { withAuthenticator } from 'aws-amplify-react-native'
+Amplify.configure({
+  ...config,
+  Analytics: {
+    disabled: true
+  }
+}
+)
+
+function App() {
   return (
     <NavigationContainer>
-        <RootNavigator/>
-        <StatusBar style="light" />
+      <RootNavigator />
+      <StatusBar style="auto" />
     </NavigationContainer>
   );
 }
-
+export default withAuthenticator(App)
 const styles = StyleSheet.create({
   container: {
     flex: 1,
